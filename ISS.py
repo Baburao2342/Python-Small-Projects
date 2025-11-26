ecc98340469219447885c90ef694ab1da289ea16
Parijat Basak <parijatbasak06@gmail.com>
2025-11-27 03:40:58 +0530

    ISS Live Position! Enjoy!

---
+++ ISS.py
@@ -0,0 +1,16 @@
+import requests     # Install requests library
+
+url = 'http://api.open-notify.org/iss-now.json'
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+
+print('''
+
+ISS LIVE POSITION!
+
+''')
+
+print(f"Latitude: {data['iss_position']['latitude']}")
+print(f"Longitude: {data['iss_position']['longitude']}\n")
